ba22e32cbfaa74bdaa4c7c98d7a5fd72bd23b661
ahmedsujon <sujonahmed424@gmail.com>
2022-07-15 04:41:29 -0700

    chnages

--- resources/views/livewire/home-component.blade.php
+++ resources/views/livewire/home-component.blade.php
@@ -3,7 +3,7 @@
     <div class="col-lg-6 mx-auto">
         <p class="lead mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium qui a, consectetur
             necessitatibus dolores cumque saepe quaerat totam, minus porro velit, beatae modi cum nobis eum. Earum
-            similique dolor veniam. update</p>
+            similique dolor veniam.</p>
         <div class="d-grid gap-3 d-sm-flex justify-content-sm-center">
             <a style="padding: 20px; width: 300px;" href="{{ route('database.search') }}"
                 class="btn btn-primary btn-lg px-4 gap-3">Beginner Search</a>
