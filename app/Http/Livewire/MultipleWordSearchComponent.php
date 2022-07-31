531218e4d25383bb7952ab02d6464f25852a2a68
nz-hridoy <nayeemuzaman05@gmail.com>
2022-08-01 00:43:48 +0600

    Done, Chill

--- app/Http/Livewire/MultipleWordSearchComponent.php
+++ app/Http/Livewire/MultipleWordSearchComponent.php
@@ -25,32 +25,36 @@ class MultipleWordSearchComponent extends Component
 
     public function render()
     {
-        $multiple_words_search = AyatWord::where('arabic_root_word', 'like', '%' . $this->multipleWordSearch . '%')
-            ->orWhere('arabic_root_word', 'like', '%' . $this->multipleWordSearchTwo . '%')
-            ->orWhere('arabic_root_word', 'like', '%' . $this->multipleWordSearchThree . '%')->get();
+        $allData = collect([]);
+        if ($this->multipleWordSearch != null || $this->multipleWordSearchTwo != null || $this->multipleWordSearchThree != null) {
+            if ($this->multipleWordSearch != null) {
+                $multiple_words_search = AyatWord::where('arabic_root_word', 'like', '%' . $this->multipleWordSearch . '%')->get();
+                foreach ($multiple_words_search as $result) {
+                    $allData->push($result);
+                }
+            }
+            if ($this->multipleWordSearchTwo != null) {
+                $multiple_words_search2 = AyatWord::where('arabic_root_word', 'like', '%' . $this->multipleWordSearchTwo . '%')->get();
+                foreach ($multiple_words_search2 as $result2) {
+                    $allData->push($result2);
+                }
+            }
+            if ($this->multipleWordSearchThree != null) {
+                $multiple_words_search3 = AyatWord::where('arabic_root_word', 'like', '%' . $this->multipleWordSearchThree . '%')->get();
+                foreach ($multiple_words_search3 as $result3) {
+                    $allData->push($result3);
+                }
+            }
+        } else {
+            $multiple_words_searchall = AyatWord::all();
+            foreach ($multiple_words_searchall as $resultall) {
+                $allData->push($resultall);
+            }
+        }
+
+        $multiple_words_search = $allData->sortBy('surah_no')->paginate($this->sortingValue);
 
 
-        // $multiple_words_search_two = AyatWord::where('arabic_root_word', 'like', '%' . $this->multipleWordSearchTwo . '%')->get();
-        // $multiple_words_search_three = AyatWord::where('arabic_root_word', 'like', '%' . $this->multipleWordSearchThree . '%')->get();
-
-
-        // $multiple_words_search_tab_two = AyatWord::where('arabic_root_word', 'like', '%' . $this->multipleWordSearchTabTwo . '%')->get();
-        // $multiple_words_search_tab_three = AyatWord::where('arabic_root_word', 'like', '%' . $this->multipleWordSearchTabThree . '%')->get();
-
-        // $multiple_words_search = collect($multiple_words_search);
-        // $multiple_words_search_two = collect($multiple_words_search_two);
-        // $multiple_words_search_three = collect($multiple_words_search_three);
-
-        // $multiple_words_search->merge($multiple_words_search_two);
-        // $multiple_words_search->merge($multiple_words_search_three);
-
-        // dd($multiple_words_search);
-
-
-        return view(
-            'livewire.multiple-word-search-component',
-            ['multiple_words_search' => $multiple_words_search]
-        )
-            ->layout('layouts.base');
+        return view('livewire.multiple-word-search-component', ['multiple_words_search' => $multiple_words_search])->layout('layouts.base');
     }
 }
